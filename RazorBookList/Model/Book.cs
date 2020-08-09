11e609d6e31b83b80a0b69392bea014e4e92410d
Doid <24828728+Gorillis@users.noreply.github.com>
2020-08-09 21:29:53 +0100

    Add project files.

---
+++ RazorBookList/Model/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorBookList.Model
+{
+    public class Book
+    {
+        //Set ID as identity column with [Key]
+        [Key]
+        public int Id { get; set; }
+
+        //Required properties cannot be null
+        [Required]
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+
+        public string ISBN { get; set; }
+    }
+}
